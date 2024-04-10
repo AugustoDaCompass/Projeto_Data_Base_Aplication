94ac195ac1d145b32af21fab50952e96b24c876e
Your Name <you@example.com>
2024-04-10 16:36:22 -0300

    projetodata

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import psycopg2
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/users', methods=['POST'])
+def adicionar_usuario():
+    try:
+        data = request.json
+        nome = data.get('nome')
+        # Supondo que você tenha outras colunas como "email", "idade", etc.
+        habilidade = data.get('habilidade')
+        sexo = data.get('sexo')
+
+        # Conectar ao banco de dados
+        conn = psycopg2.connect(
+            dbname='quilombo',
+            user='postgres',
+            password='mariojol',
+            host='localhost'
+        )
+        cur = conn.cursor()
+
+        # Executar a inserção na tabela de usuários
+        cur.execute('INSERT INTO guerreiro (nome, habilidade, sexo) VALUES (%s, %s, %s)', (nome, habilidade,sexo,))
+        
+        # Commit da transação e fechamento da conexão
+        conn.commit()
+        conn.close()
+
+        return jsonify({'message': 'Usuário adicionado com sucesso'}), 201
+
+    except psycopg2.Error as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
